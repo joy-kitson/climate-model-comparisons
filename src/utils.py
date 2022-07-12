def798cff6d61c77697177d58e883d3af4db9c64
Joy Kitson <jkitson@umd.edu>
2022-07-12 11:59:02 -0400

    added a function to save a list as lines in a file

--- src/utils.py
+++ src/utils.py
@@ -9,3 +9,7 @@ def save_or_show(save_as=None):
 def read_lines(path):
     with open(path, 'r') as in_file:
         return [l.strip() for l in in_file]
+
+def write_lines(lines, path):
+    with open(path, 'w') as out_file:
+        out_file.writelines([l + '\n' for l in lines])
