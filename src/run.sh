afc889c02fdf0212d10d006849510dc68d7f6b78
Joy Kitson <jkitson@umd.edu>
2022-09-27 19:53:24 -0400

    switched dir to bring a commandline argument and added ncl scripts to run.sh

--- src/run.sh
+++ src/run.sh
@@ -1,10 +1,28 @@
 #!/usr/bin/bash
 
-# This script runs all scripts needed to generate the plots and data files for the paper
+# This script runs all scripts needed to generate the plots and data files
+# for the paper
 
-./plot_rankings_diffs.py -o ../data
-./plot_cos_sim.py -o ../data
+# Echos the command before running it, similar to make
+run () {
+  echo $@
+  $@
+  echo
+}
+
+DATA_DIR="../data/"
+
+# Generates suplimental data files used by other scripts from
+# unweighted_data_37gcms_66metrics_0708.nc
+run ncl generate_data.ncl dir\='"'${DATA_DIR}'"'
+
+run ncl radius_of_similarity.ncl dir\='"'${DATA_DIR}'"'
+run ncl heatmap_sample.ncl dir\='"'${DATA_DIR}'"'
+
+run ./plot_rankings_diffs.py -o ${DATA_DIR}
+run ./plot_cos_sim.py -o ${DATA_DIR}
 
 for prog in circo dot sfdp ; do
-  ./plot_network.py ../data/model_weighted_data_cos_sim_region*.csv -t .8 -a -p ${prog}
+  run ./plot_network.py ${DATA_DIR}/model_weighted_data_cos_sim_region*.csv \
+    -t .8 -a -p ${prog}
 done
